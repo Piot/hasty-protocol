f5a9372e770deb43055acfacfaa003ed6ddd110b
Peter Bjorklund <piot@hotmail.com>
2017-05-03 12:26:03 +0200

    Cleaned up publish stream deserialize somewhat

--- packetdeserializers/publish_stream_deserialize.go
+++ packetdeserializers/publish_stream_deserialize.go
@@ -2,8 +2,10 @@ package packetdeserializers
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/piot/hasty-protocol/commands"
+	"github.com/piot/hasty-protocol/deserialize"
 	"github.com/piot/hasty-protocol/packet"
 )
 
@@ -14,12 +16,23 @@ func ToPublishStream(in packet.Packet) (commands.PublishStream, error) {
 	}
 
 	payload := in.Payload()
-	channel, idOctets, err := ToChannelID(payload)
+	pos := 0
+	channel, idOctets, err := ToChannelID(payload[pos:])
 	if err != nil {
 		return commands.PublishStream{}, err
 	}
+	pos += idOctets
 
-	chunk := payload[idOctets:]
+	payloadLength, payloadLengthOctetsUsed, payloadLengthErr := deserialize.ToSmallLength(payload[pos:])
+	if payloadLengthErr != nil {
+		return commands.PublishStream{}, payloadLengthErr
+	}
+	pos += payloadLengthOctetsUsed
+
+	chunk := payload[pos:]
+	if len(chunk) != int(payloadLength) {
+		return commands.PublishStream{}, fmt.Errorf("Illegal length %d expected %d", len(chunk), int(payloadLength))
+	}
 
 	createdPublishStream := commands.NewPublishStream(channel, chunk)
 	return createdPublishStream, nil
